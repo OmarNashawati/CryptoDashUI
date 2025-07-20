7db9da768253a5cdcecb8ff94f64bd222f8f2c19
Omar Nashawati <oceanprime.on@gmail.com>
2025-07-20 09:48:03 +0300

    prepered to build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crypto-dash-ui/',
   plugins: [
     vue(),
     vueDevTools(),
